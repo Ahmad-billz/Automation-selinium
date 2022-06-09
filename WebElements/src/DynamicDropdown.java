9a058ec31de62aff27d75992797030c9b082efdd
Ahmad-billz <ahmadbilalsohail@hotmail.com>
2022-06-09 19:23:57 +0200

    parent-child instead of indexing

--- WebElements/src/DynamicDropdown.java
+++ WebElements/src/DynamicDropdown.java
@@ -22,7 +22,7 @@ public class DynamicDropdown {
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 		driver.findElement(By.xpath("//a[@value='BLR']")).click();		
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
 
 		//    //a[@value='MAA'] xpath for chennai
 		//    //a[@value='BLR'] xpath for Bangluru
